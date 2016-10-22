507d6813603618c64b985ba45327285390be4ef1
crm84 <christopher.ryan.mitchell@outlook.com>
2016-10-22 16:24:01 -0400

    Add files via upload

--- plot1.R
+++ plot1.R
@@ -3,7 +3,13 @@ source("./Helpers.R")
 getData()
 df <- loadData()
 
-hist(df$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red")
+df$Date <- as.Date(df$Date, format = "%d/%m/%Y")
 
-dev.copy(png,"plot1.png", width=480, height=480)
+df <- df[df$Date >= as.Date("2007-02-01") & df$Date <= as.Date("2007-02-02"),]
+df$DateTime <- strptime(paste(df$Date, df$Time, sep = " "), format = "%Y-%m-%d %H:%M:%S")
+
+
+png("plot1.png", width=480, height=480)
+hist(df$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red" )
+
 dev.off()
--- plot4.R
+++ plot4.R
@@ -1,3 +1,4 @@
+
 source("./Helpers.R")
 
 getData()
@@ -5,24 +6,24 @@ getData()
 df <- loadData()
 
 
-subdf <- df[df$Date %in% c("1/2/2007","2/2/2007") ,]
+df <- df[df$Date %in% c("1/2/2007","2/2/2007") ,]
 
-datetime <- strptime(paste(subdf$Date, subdf$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+datetime <- strptime(paste(df$Date, df$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 
 
 
 png("plot4.png", width=480, height=480)
 par(mfrow = c(2, 2)) 
 
-plot(datetime, subdf$Global_active_power, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+plot(datetime, df$Global_active_power, type="l", xlab="", ylab="Global Active Power", cex=0.2)
 
-plot(datetime, subdf$Voltage, type="l", xlab="datetime", ylab="Voltage")
+plot(datetime, df$Voltage, type="l", xlab="datetime", ylab="Voltage")
 
-plot(datetime, subdf$Sub_metering_1, type="l", col="black", xlab="", ylab="Energy sub metering")
-lines(datetime, subdf$Sub_metering_2, type="l", col="red")
-lines(datetime, subdf$Sub_metering_3, type="l", col="blue")
-legend("topright", col=c("black", "red", "blue"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1)
+plot(datetime, df$Sub_metering_1, type="l", col="black", xlab="", ylab="Energy sub metering")
+lines(datetime, df$Sub_metering_2, type="l", col="red")
+lines(datetime, df$Sub_metering_3, type="l", col="blue")
+legend("topright", col=c("black", "red", "blue"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, bty = "n")
 
-plot(datetime, subdf$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
+plot(datetime, df$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
 
 dev.off()
